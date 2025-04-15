c382b8c7a41b6b16a2480f9695f91efb01ae6bec
Shrouk21 <158491651+Shrouk21@users.noreply.github.com>
2025-04-15 18:14:10 +0200

    Update

--- decoder.py
+++ decoder.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 class Decoder(nn.Module):
     def __init__(self, expansive_input_channels, contractive_input_channels, n_filters=32):
         super(Decoder, self).__init__()
-        self.up = nn.ConvTranspose2d(expansive_input_channels, n_filters, kernel_size=2, stride=2, padding=1)
+        self.up = nn.ConvTranspose2d(expansive_input_channels, n_filters, kernel_size=3, stride=2, padding=1)
         self.conv1 = nn.Conv2d(n_filters + contractive_input_channels, n_filters, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(n_filters, n_filters, kernel_size=3, padding=1)
         self.relu = nn.ReLU()
