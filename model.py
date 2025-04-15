12c365bf6b152dd12b7956d342596414d2973682
Shrouk21 <158491651+Shrouk21@users.noreply.github.com>
2025-04-15 17:31:58 +0200

    Update model.py

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ from encoder import Encoder
 
 
 class UNet(pl.LightningModule):
-    def __init__(self, input_channels, n_filters, n_classes, lr=1e-3):
+    def __init__(self, input_channels, n_filters=32, n_classes, lr=1e-3):
         super(UNetLightning, self).__init__()
         self.save_hyperparameters()
         
