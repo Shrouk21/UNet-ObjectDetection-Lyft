51ebef811f9d542c50d439d4639f8c3eec75016a
Shrouk21 <158491651+Shrouk21@users.noreply.github.com>
2025-04-18 16:29:28 +0200

    Update

--- datas.py
+++ datas.py
@@ -14,7 +14,7 @@ class DataProcessing(Dataset):
         self.image_dir = image_dir
         self.mask_dir = mask_dir
         self.class_map = class_map
-        self.transfrom = A.Compose([
+        self.transform = A.Compose([
             A.Resize(512, 512),
             A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
             ToTensorV2()
