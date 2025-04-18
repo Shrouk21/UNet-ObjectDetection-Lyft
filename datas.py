f8817ee90d5cac3eff1e46cb8226eac2bffbe879
Shrouk21 <158491651+Shrouk21@users.noreply.github.com>
2025-04-18 15:45:45 +0200

    Update

--- datas.py
+++ datas.py
@@ -71,31 +71,34 @@ def __getitem__(self, idx):
         # return mapped_mask, mapping
 
 class Dataloader(pl.LightningDataModule):
-    def __init__(self, data_dir, batch_size=32, num_workers=4, val_split=0.2):
+    def __init__(self, data_dir, batch_size=32, num_workers=4, val_split=0.2, test_split=0.1):
         super().__init__()
         self.data_dir = data_dir
         self.batch_size = batch_size
         self.num_workers = num_workers
         self.val_split = val_split
+        self.test_split = test_split
 
-    def setup(self, stage=None):
-        train_image_dir = os.path.join(self.data_dir, "train_val", "images")
-        train_mask_dir = os.path.join(self.data_dir, "train_val", "masks")
+    def setup(self, img_subpath, mask_subpath,stage=None):
+        train_image_dir = os.path.join(self.data_dir, img_subpath)
+        train_mask_dir = os.path.join(self.data_dir, mask_subpath)
     
     
         print("Looking for training images in:", train_image_dir)
         print("Looking for training masks in:", train_mask_dir)
     
-        full_dataset = DataProcessing(image_dir=train_image_dir, mask_dir=train_mask_dir, transform=self.transform)
-        val_size = int(self.val_split * len(full_dataset))
-        train_size = len(full_dataset) - val_size
-        self.train_dataset, self.val_dataset = random_split(full_dataset, [train_size, val_size])
+        full_dataset = DataProcessing(image_dir=train_image_dir, mask_dir=train_mask_dir)
+        dataset_size = len(full_dataset)
+        val_size = int(self.val_split * dataset_size)
+        test_size = int(self.test_split * dataset_size)
+        train_size = dataset_size - val_size - test_size
+        self.train_dataset, self.val_dataset, self.test_dataset = random_split(full_dataset, [train_size, val_size, test_size])
 
 
         
-        test_image_dir = os.path.join(self.data_dir, "TEST", "images")
-        test_mask_dir = os.path.join(self.data_dir, "TEST", "masks")
-        self.test_dataset = DataProcessing(image_dir=test_image_dir, mask_dir=test_mask_dir, transform=self.transform)
+        # test_image_dir = os.path.join(self.data_dir, "TEST", "images")
+        # test_mask_dir = os.path.join(self.data_dir, "TEST", "masks")
+        # self.test_dataset = DataProcessing(image_dir=test_image_dir, mask_dir=test_mask_dir, transform=self.transform)
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
